b1f5414c8334c2600225bc4afa9d0e610431deb7
Raj Kumar <interplaytheheads@gmail.com>
2021-02-10 16:08:12 +0200

    Get method done

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using ConsumeAPI.Models;
+
+namespace ConsumeAPI.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            IEnumerable<StudentDisplay> student = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44373/api/");
+                var responseTask = client.GetAsync("student");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if(result.IsSuccessStatusCode)
+                {
+                    var readJob = result.Content.ReadAsAsync<IList<StudentDisplay>>();
+                    readJob.Wait();
+                    student = readJob.Result;
+                }
+                else
+                {
+                    student = Enumerable.Empty<StudentDisplay>();
+                    ModelState.AddModelError(string.Empty, "Server error occured. Please contact admin for help!");
+                }
+            }
+            return View(student);
+        }
+    }
+}
